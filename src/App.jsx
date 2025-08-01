192c2e6edef0d9426fd5790d5d53946985c598fc
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2025-07-31 21:52:53 -0600

    added the main logic and modularized the code

---
+++ src/App.jsx
@@ -0,0 +1,111 @@
+import "./App.css";
+import OrderDetails from "./components/OrderDetails";
+import Item from "./components/Item";
+
+function App() {
+  const items = [
+    {
+      id: 1,
+      photo: "real_madrid.webp",
+      name: "Real Madrid",
+      price: 119.99,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+    {
+      id: 2,
+      photo: "milan.png",
+      name: "Milan",
+      price: 99.99,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+    {
+      id: 3,
+      photo: "chelsea.webp",
+      name: "Chelsea",
+      price: 99.99,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+    {
+      id: 4,
+      photo: "barcelona.png",
+      name: "Barcelona",
+      price: 109.99,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+    {
+      id: 5,
+      photo: "benfica.png",
+      name: "Benfica",
+      price: 89.49,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+    {
+      id: 6,
+      photo: "manchester.webp",
+      name: "Manchester City",
+      price: 129.79,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+    {
+      id: 7,
+      photo: "bayern.webp",
+      name: "Bayern",
+      price: 119.99,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+    {
+      id: 8,
+      photo: "psg.png",
+      name: "PSG",
+      price: 94.99,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+    {
+      id: 9,
+      photo: "ajax.webp",
+      name: "Ajax",
+      price: 89.99,
+      active: false,
+      quantity: 1,
+      isInBag: false,
+    },
+  ];
+  const itemsInBag = items.filter((item) => item.isInBag);
+  const shopName = "Vinny Shop Made with React JS";
+
+  return (
+    <>
+      <section className="items">
+        <h4>{shopName}</h4>
+
+        {items.map((item) => (
+          <Item
+            selectProduct={(id) => alert(`clicked product ${id}`)}
+            item={item}
+            key={item.id}
+          />
+        ))}
+      </section>
+
+      <OrderDetails />
+    </>
+  );
+}
+
+export default App;
